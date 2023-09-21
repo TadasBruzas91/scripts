0e9647ff351b95689f4d59e1ceed133c839ebbdb
Tadas Bružas <tadasbruzas91@gmail.com>
2023-09-21 11:00:31 +0300

    Add script to fix pacman gpg errora

---
+++ fix-pacman-gpg-errors.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo rm -r /etc/pacman.d/gnupg
+sudo pacman-key --init
+sudo pacman-key --populate archlinux 
+sudo pacman-key --refresh-keys
+sudo pacman -Sy gnupg archlinux-keyring 
+sudo pacman -Sc
